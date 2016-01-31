60d36ceffc2dda6ca1ca933bce9e53cbf4efebd2
Yerols <Kibbel8@gmail.com>
2016-01-31 13:33:07 +0100

    First check in for the program, it cant do a lot

---
+++ Project Doorbell/StateObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Doorbell
+{
+    public class StateObject
+    {
+        // Client socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int BUFFER_SIZE = 1024;
+        // Receive buffer.
+        public byte[] buffer = new byte[BUFFER_SIZE];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+}
